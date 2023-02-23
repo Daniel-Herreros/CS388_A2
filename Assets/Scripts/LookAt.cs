a08adad928164c2c939fb99a4bf01302cd6b2ffd
Daniel-Herreros <d.herreros@digipen.edu>
2023-02-23 20:15:27 +0100

    Added menu with VR

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LookAt : MonoBehaviour
+{
+
+
+    public GameObject camController;
+
+    //move variables
+    Vector3 movPosition;
+    Vector3 camPosition;
+    GameObject hitObject;
+
+    int colliderId;
+
+    GameObject prevHitObject;
+
+
+    // Start is called before the first frame update
+    void Start(){
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        SetDirection();
+
+    }
+
+    void SetDirection(){
+        Vector3 fwd = camController.transform.forward;
+        RaycastHit hit;
+        if (Physics.Raycast(camController.transform.position, fwd, out hit))
+        {
+            if (hit.collider.tag == "Button")
+            {
+                bool finish = false;
+                if (hit.colliderInstanceID == colliderId)
+                {
+                    Color rend = hit.collider.gameObject.GetComponent<Button>().image.color;
+                    hit.collider.gameObject.GetComponent<Button>().image.color = new Color(rend.r, rend.g-0.1f, rend.b - 0.1f, rend.a);
+                    if(rend.g <= 0.0f)
+                    {
+                        finish = true;
+                    }
+                    prevHitObject = hit.collider.gameObject;
+                }
+                else
+                {
+                    if (prevHitObject){
+                        prevHitObject.GetComponent<Button>().image.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                    }
+                }
+
+                colliderId = hit.colliderInstanceID;
+                if (finish){
+                    if (hitObject){
+                        hitObject.GetComponent<Button>().image.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                    }
+                    hitObject = hit.collider.gameObject;
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+
+                }
+                Debug.DrawRay(camController.transform.position, fwd, Color.green, Mathf.Infinity);
+            }
+            else
+            {
+                Debug.DrawRay(camController.transform.position, fwd, Color.red, Mathf.Infinity);
+                if (prevHitObject){
+                    prevHitObject.GetComponent<Collider>().gameObject.GetComponent<Button>().image.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                }
+            }
+        }
+    }
+}
